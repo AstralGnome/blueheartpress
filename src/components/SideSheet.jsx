fd8a8ad0be8538e861bb0d777c23fc3005a0f4fc
Astral Gnome <reedkeenan@gmail.com>
2022-02-03 11:57:23 -0800

    Added delayed animation to information button.

--- src/components/SideSheet.jsx
+++ src/components/SideSheet.jsx
@@ -2,6 +2,7 @@ import { Paragraph, SideSheet, Position } from 'evergreen-ui';
 import React from 'react';
 import "../style/Slider.scss"
 import {BsFillInfoCircleFill} from "react-icons/bs"
+import "animate.css"
 
 function SideSheetComponent() {
 
@@ -12,7 +13,7 @@ const [isShown, setIsShown] = React.useState(false)
       <SideSheet position={Position.LEFT} isShown={isShown} onCloseComplete={() => setIsShown(false)}>
         <Paragraph margin={40}>Basic Example</Paragraph>
       </SideSheet>
-      <BsFillInfoCircleFill className="SideSheetButton" onClick={() => setIsShown(true)}/>
+      <BsFillInfoCircleFill className="SideSheetButton animate__animated animate__heartBeat animate__delay-2s animate__slower" onClick={() => setIsShown(true)}/>
     </>
   )
 }
