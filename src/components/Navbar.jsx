f13d49c8810ed84f49d7c80c3b17a899c831c06c
Astral Gnome <reedkeenan@gmail.com>
2022-02-07 15:57:29 -0800

    Finished Styling Navbar.

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,9 +1,9 @@
-import { CircleArrowRightIcon, EditIcon, IconButton, Menu, MenuIcon, PeopleIcon, Popover, TrashIcon } from 'evergreen-ui';
+import { CircleArrowRightIcon, EditIcon, GlobeIcon, HandIcon, HomeIcon, IconButton, LogOutIcon, Menu, MenuIcon, PeopleIcon, Popover, ShareIcon, TrashIcon } from 'evergreen-ui';
 import React from 'react';
+import { BsFillPersonFill, BsFillShareFill, BsPerson } from 'react-icons/bs';
 import { Link } from 'react-router-dom';
 import { position } from 'ui-box';
 import "../style/Navbar.scss"
-
 
 function Navbar() {
   return (
@@ -11,17 +11,21 @@ function Navbar() {
     <Popover
   position={position.BOTTOM_LEFT}
   content={
-    <Menu>
-      <div className="Menu" title="Navigation">
-        <Link to="/"><Menu.Item className="Menu" icon={PeopleIcon}>Home</Menu.Item></Link>
-        <Link to="/about"><Menu.Item className="Menu" icon={CircleArrowRightIcon}>About</Menu.Item></Link>
-        <Link to="/contact"><Menu.Item className="Menu" icon={EditIcon}>Contact</Menu.Item></Link>
+    <Menu >
+      <div className="Menu">
+        <div className="MenuHeading" title="Navigation">Navigation</div>
+        <Link className="LinkText" to="/"><HomeIcon size={24} className="MenuIcon"/>Home</Link>
+        <Link className="LinkText" to="/about"><HandIcon size={24} className="MenuIcon"/>About</Link>
+        <Link className="LinkText" to="/contact"><GlobeIcon size={24} className="MenuIcon"/>Contact</Link>
+        <div className="Spacer"></div>
       </div>
-      <Menu.Group title="Actions">
-        <Link to="/profile"><Menu.Item className="Menu" icon={PeopleIcon}>Logout</Menu.Item></Link>
-        <Link to="/"><Menu.Item className="Menu" icon={PeopleIcon}>Share</Menu.Item></Link>
-        <Menu.Item className="Menu" icon={EditIcon} intent="danger">Edit</Menu.Item>
-      </Menu.Group>
+      <div className="Menu">
+      <div className="MenuHeading" title="Actions">Actions</div>
+        <Link className="LinkText" to="/"><ShareIcon size={24} className="MenuIcon"/>Share</Link>
+        <Link className="LinkText" to="/about"><LogOutIcon size={24} className="MenuIcon"/>Logout</Link>
+        <Link className="LinkText" to="/"><EditIcon size={24} className="MenuIcon"/>Edit</Link>
+        <div className="Spacer"></div>
+      </div>
     </Menu>
   }
 >
