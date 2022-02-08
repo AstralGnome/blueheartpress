0ea664bacf8c5b6d3c225253adc1dd9a67c57db0
Astral Gnome <reedkeenan@gmail.com>
2022-02-07 16:03:37 -0800

    Added Favorite action to Actions in the Navbar.

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,4 +1,4 @@
-import { CircleArrowRightIcon, EditIcon, GlobeIcon, HandIcon, HomeIcon, IconButton, LogOutIcon, Menu, MenuIcon, PeopleIcon, Popover, ShareIcon, TrashIcon } from 'evergreen-ui';
+import { CircleArrowRightIcon, EditIcon, GlobeIcon, HandIcon, HomeIcon, IconButton, InfoSignIcon, LogOutIcon, Menu, MenuIcon, PeopleIcon, Popover, ShareIcon, StarEmptyIcon, TrashIcon, UserIcon } from 'evergreen-ui';
 import React from 'react';
 import { BsFillPersonFill, BsFillShareFill, BsPerson } from 'react-icons/bs';
 import { Link } from 'react-router-dom';
@@ -15,14 +15,15 @@ function Navbar() {
       <div className="Menu">
         <div className="MenuHeading" title="Navigation">Navigation</div>
         <Link className="LinkText" to="/"><HomeIcon size={24} className="MenuIcon"/>Home</Link>
-        <Link className="LinkText" to="/about"><HandIcon size={24} className="MenuIcon"/>About</Link>
-        <Link className="LinkText" to="/contact"><GlobeIcon size={24} className="MenuIcon"/>Contact</Link>
+        <Link className="LinkText" to="/about"><InfoSignIcon size={24} className="MenuIcon"/>About</Link>
+        <Link className="LinkText" to="/contact"><HandIcon size={24} className="MenuIcon"/>Contact</Link>
         <div className="Spacer"></div>
       </div>
       <div className="Menu">
       <div className="MenuHeading" title="Actions">Actions</div>
+        <Link className="LinkText" to="/"><StarEmptyIcon size={24} className="MenuIcon"/>Favorite</Link>
         <Link className="LinkText" to="/"><ShareIcon size={24} className="MenuIcon"/>Share</Link>
-        <Link className="LinkText" to="/about"><LogOutIcon size={24} className="MenuIcon"/>Logout</Link>
+        <Link className="LinkText" to="/about"><UserIcon size={24} className="MenuIcon"/>Logout</Link>
         <Link className="LinkText" to="/"><EditIcon size={24} className="MenuIcon"/>Edit</Link>
         <div className="Spacer"></div>
       </div>
