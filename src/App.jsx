c15b4fb41dc28ead10120c620e4d42e5b05efb64
Astral Gnome <reedkeenan@gmail.com>
2022-02-10 00:31:35 -0800

    Styling and function upgrades to homepage.

--- src/App.jsx
+++ src/App.jsx
@@ -9,21 +9,22 @@ import Profile from "./pages/Profile";
 import Navbar from "./components/Navbar";
 import Comic from "./pages/Comic";
 import Home from "./pages/Home";
+
 function App() {
   return (
       <Router>
         <Navbar/>
-            <Routes>
-              <Route path="/" element={<Home/>}/>
-              <Route path="/user" element={<Comic/>}/>
-              <Route path="/contact" element={<Contact/>}/>
-              <Route path="/about" element={<About/>} component={About}/>
-              <Route path="/about/me" element={<AboutMe/>}/>
-              <Route path="/about/you" element={<AboutYou/>}/>
-              <Route path="/profile" element={<Profile/>}/>
-              <Route path="/profile/:username" element={<Profile/>}/>
-              <Route path="*" element={<ErrorPage/>}/>
-            </Routes>
+          <Routes>
+            <Route path="/" element={<Home/>}/>
+            <Route path="/user" element={<Comic/>}/>
+            <Route path="/contact" element={<Contact/>}/>
+            <Route path="/about" element={<About/>} component={About}/>
+            <Route path="/about/me" element={<AboutMe/>}/>
+            <Route path="/about/you" element={<AboutYou/>}/>
+            <Route path="/profile" element={<Profile/>}/>
+            <Route path="/profile/:username" element={<Profile/>}/>
+            <Route path="*" element={<ErrorPage/>}/>
+          </Routes>
       </Router>
   );
 }
