3196e527b43b8d24d50c9127fb3363cde92477a0
Astral Gnome <reedkeenan@gmail.com>
2022-02-10 22:15:43 -0800

    deploy push

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import "./style/App.scss"
+import "./App.css"
 import {BrowserRouter as Router, Routes, Route} from "react-router-dom"
 import Contact from "./pages/Contact";
 import AboutMe from "./pages/AboutMe";  
