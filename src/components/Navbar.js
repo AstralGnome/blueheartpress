bd712600bbeb33d9525f1c4639aa2111aa6ff000
Astral Gnome <reedkeenan@gmail.com>
2022-02-13 23:05:41 -0800

    troubleshooting nav

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,4 +1,4 @@
-import { EditIcon, HandIcon, HomeIcon, IconButton, InfoSignIcon, Menu, MenuIcon, PersonIcon, Popover, ShareIcon, StarEmptyIcon, UserIcon } from 'evergreen-ui';
+import { HandIcon, HomeIcon, IconButton, InfoSignIcon, Menu, MenuIcon, PersonIcon, Popover } from 'evergreen-ui';
 import React from 'react';
 import { Link } from 'react-router-dom';
 import { position } from 'ui-box';
@@ -13,18 +13,18 @@ function Navbar() {
     <Menu>
       <div className="Menu">
         <div className="MenuHeading" title="Navigation">Navigation</div>
-        <Link className="LinkText" to="/"><HomeIcon size={24} className="MenuIcon"/>Home</Link>
-        <Link className="LinkText" to="/about"><InfoSignIcon size={24} className="MenuIcon"/>About</Link>
-        <Link className="LinkText" to="/contact"><HandIcon size={24} className="MenuIcon"/>Contact</Link>
-        <Link className="LinkText" to="/user"><PersonIcon size={24} className="MenuIcon"/>User Page</Link>
+        <Link className="LinkText" to="/"></Link>
+        <Link className="LinkText" to="/about"></Link>
+        <Link className="LinkText" to="/contact"></Link>
+        <Link className="LinkText" to="/user"></Link>
         <div className="Spacer"></div>
       </div>
       <div className="Menu">
       <div className="MenuHeading" title="Actions">Actions</div>
-        <Link className="LinkText" to="/user"><StarEmptyIcon size={24} className="MenuIcon"/>Favorite</Link>
-        <Link className="LinkText" to="/user"><ShareIcon size={24} className="MenuIcon"/>Share</Link>
-        <Link className="LinkText" to="/user"><UserIcon size={24} className="MenuIcon"/>Logout</Link>
-        <Link className="LinkText" to="/user"><EditIcon size={24} className="MenuIcon"/>Edit</Link>
+        <Link className="LinkText" to="/user"></Link>
+        <Link className="LinkText" to="/user"></Link>
+        <Link className="LinkText" to="/user"></Link>
+        <Link className="LinkText" to="/user"></Link>
         <div className="Spacer"></div>
       </div>
     </Menu>
