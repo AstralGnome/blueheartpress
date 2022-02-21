8508c4536f985326bfcfd0721c3529ec30e12273
Astral Gnome <reedkeenan@gmail.com>
2022-02-21 00:59:45 -0800

    Testing slider deploy.

--- src/pages/Comic.js
+++ src/pages/Comic.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import Banner from '../components/Banner';
-// import ImageSlider from '../components/ImageSlider/ImageSlider';
-// import { SliderData } from '../components/ImageSlider/SliderData';
+import ImageSlider from '../components/ImageSlider/ImageSlider';
+import { SliderData } from '../components/ImageSlider/SliderData';
 import Wrapper from '../components/Wrapper';
 import "../style/Comic.css"
 
@@ -11,7 +11,7 @@ function Comic() {
     <div className = "Comic">
       <Wrapper>
         <Banner/>
-        {/* <ImageSlider slides={SliderData}/> */}
+        <ImageSlider slides={SliderData}/>
       </Wrapper>
     </div>
   )
