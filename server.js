ecf2c7777ae94a21ebccaa6e2bda2f7d88f63f49
Astral Gnome <reedkeenan@gmail.com>
2022-03-10 23:59:36 -0800

    Got express server connected and configured and printing to console.

--- server.js
+++ server.js
@@ -1,22 +1,46 @@
 const express       = require('express');
 const app           = express();
-const connection    = require('./dbfiles/dbConfig');
+const db            = require('./dbfiles/dbConfig');
 
-app.get('/', function(req,res) {
-  let sql = "SELECT * FROM USER_INFO"
-  connection.query(sql, function(err, results) {
+db.connect ((err) => {
+  if(err) {
+    throw err;
+  }
+  console.log('MySql Connecticated!')
+})
+
+app.get('/', (req,res) => {
+  let sql = "SELECT * FROM user_info"
+   db.query(sql, (err, results) => {
     if (err) throw err;
     res.send(results);
-    console.log("results")
+    console.log(results);
   })
-  console.log('Called');
-  res.send('Hellooooo');
 });
 
-app.listen(3001, function() {
-  console.log('App listening on PoRtt 3000')
-  connection.connect(function(err){
-    if(err) throw err;
-    console.log('Database Connected!')
-  })
-});
+app.listen('3001', () => {
+  console.log('Server started on port 3001')
+})
+
+
+
+
+// app.get('/', function(req,res) {
+//   let sql = "SELECT * FROM user_info"
+  
+//   connection.query(sql, function(err, results) {
+//     if (err) throw err;
+//     res.send(results);
+//     console.log(results);
+//   })
+//   console.log('Called');
+//   res.send('Hellooooo');
+// });
+
+// app.listen(3001, function() {
+//   console.log('App listening on PoRtt 3000')
+//   connection.connect(function(err){
+//     if(err) throw err;
+//     console.log('Database Connecticated!')
+//   })
+// });
