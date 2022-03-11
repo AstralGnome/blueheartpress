2e71c88472258035e3e7b5d3d50d3f727d7930bb
Astral Gnome <reedkeenan@gmail.com>
2022-03-11 13:04:56 -0800

    In server.js: Created new table; inserted, updated, and deleted posts.

--- server.js
+++ server.js
@@ -2,45 +2,104 @@ const express       = require('express');
 const app           = express();
 const db            = require('./dbfiles/dbConfig');
 
+//Connect
 db.connect ((err) => {
   if(err) {
     throw err;
   }
-  console.log('MySql Connecticated!')
+  console.log('MySql Connecticated!');
+});
+
+//Create new table
+app.get('/createcommentstable', (req, res) => {
+  let sql = 'CREATE TABLE comments(id int AUTO_INCREMENT, title VARCHAR(255), body VARCHAR(255), PRIMARY KEY(id))';
+  db.query(sql, (err, results) => {
+    if(err) throw err;
+    console.log(results);
+    res.send('comments table created');
+  })
 })
 
-app.get('/', (req,res) => {
-  let sql = "SELECT * FROM user_info"
-   db.query(sql, (err, results) => {
+//Insert comment1 into new table
+
+app.get('/addcomment1', (req, res) => {
+  let post = {title: "Good comic!", body: "I liked the way the horse had his tongue hanging out."}
+  let sql ='INSERT INTO comments SET ?';
+  db.query(sql, post, (err, results) => {
+    if(err) throw err;
+    console.log(results);
+    res.send('Comment1 added.');
+  });
+});
+
+//Insert comment2 into new table
+
+app.get('/addcomment2', (req, res) => {
+  let post = {title: "Meh comic!", body: "I didn't like the way people cursed; they probably never cursed back then."}
+  let sql ='INSERT INTO comments SET ?';
+  db.query(sql, post, (err, results) => {
+    if(err) throw err;
+    console.log(results);
+    res.send('Comment2 added.');
+  });
+});
+
+//Select all comments
+
+app.get('/getcomments', (req, res) => {
+  let sql ='SELECT * FROM comments';
+  db.query(sql, (err, results) => {
+    if(err) throw err;
+    console.log(results);
+    res.send('Comments fetched.');
+  });
+});
+
+//Select individual comment.
+
+app.get('/getcomment/:id', (req, res) => {
+  let sql = `SELECT * FROM comments WHERE id = ${req.params.id}`;
+  db.query(sql, (err, result) => {
+    if(err) throw err;
+    console.log(result);
+    res.send('Comment fetched.');
+  });
+});
+
+//Update comment.
+
+app.get('/updatecomment/:id', (req, res) => {
+  let newTitle = 'Updated Title'
+  let sql = `UPDATE comments SET title = '${newTitle}' WHERE id = ${req.params.id}`;
+  db.query(sql, (err, result) => {
+    if(err) throw err;
+    console.log(result);
+    res.send('Comment updated.');
+  });
+});
+
+//Delete comment.
+
+app.get('/deletecomment/:id', (req, res) => {
+  let sql = `DELETE FROM comments WHERE id = ${req.params.id}`;
+  db.query(sql, (err, result) => {
+    if(err) throw err;
+    console.log(result);
+    res.send('Comment deleted.');
+  });
+});
+
+//Select all from user_info
+app.get('/', (req, res) => {
+  let sql = "SELECT * FROM comments"
+    db.query(sql, (err, results) => {
     if (err) throw err;
     res.send(results);
     console.log(results);
-  })
+  });
 });
 
+//Listen
 app.listen('3001', () => {
-  console.log('Server started on port 3001')
-})
-
-
-
-
-// app.get('/', function(req,res) {
-//   let sql = "SELECT * FROM user_info"
-  
-//   connection.query(sql, function(err, results) {
-//     if (err) throw err;
-//     res.send(results);
-//     console.log(results);
-//   })
-//   console.log('Called');
-//   res.send('Hellooooo');
-// });
-
-// app.listen(3001, function() {
-//   console.log('App listening on PoRtt 3000')
-//   connection.connect(function(err){
-//     if(err) throw err;
-//     console.log('Database Connecticated!')
-//   })
-// });
+  console.log('Server started on port 3001');
+});
